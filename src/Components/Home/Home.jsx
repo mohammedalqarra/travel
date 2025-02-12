3e767acf4385db7d6f59122ee17d40ceae810e43
Mohammed <mohammedalqarra2915@gmail.com>
2025-02-12 14:25:41 +0200

    Add Home & textDiv

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import video from '../../Assets/video.mp4';
+import './home.scss';
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="home">
+      <div className="overlay"></div>
+      <video src={video} muted autoPlay loop type="video/mp4"></video>
+      <div className="homeContent container">
+        <div className="textDiv">
+          <span className="smallText">Our Packages</span>
+          <h1 className="homeTitle">Search your Holiday</h1>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
